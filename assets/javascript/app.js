63a3f9e9f656b123cde367899b5e15097a401b06
TaylorRoy <tbroy@hotmail.com>
2018-09-17 14:21:52 -0600

    added syrup data display on gas station page, and working on adding pull down in syrup page for with gas station locations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,43 +2,54 @@
 // import config from './../..config.js'
 // console.log(config)
 
-let x 
+//connecting to firebase to get data that was entered on syrup page
+var config = {
+	apiKey: "AIzaSyCFaXvnaHL3JsKa8rsUtkAxFiUJBsPm9bQ",
+	authDomain: "stfirebase-66923.firebaseapp.com",
+	databaseURL: "https://stfirebase-66923.firebaseio.com",
+	projectId: "stfirebase-66923",
+	storageBucket: "stfirebase-66923.appspot.com",
+	messagingSenderId: "367723280740"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//initializing syrup variables 
+var syrupDiv;
+var cherry;
+var vanilla;
+var coconut;
+var peach;
+
+let x
 let y
 function initMap(x, y) {
 	// The location of Uluru
-	var uluru = {lat: x, lng: y};
+	var uluru = { lat: x, lng: y };
 	// The map, centered at Uluru
 	var map = new google.maps.Map(
-		document.getElementById('map'), {zoom: 4, center: uluru});
-		// The marker, positioned at Uluru
-		var marker = new google.maps.Marker({position: uluru, map: map});
+		document.getElementById('map'), { zoom: 4, center: uluru });
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({ position: uluru, map: map });
 }
 
 $(document).ready(function () {
 
 
-navigator.geolocation.getCurrentPosition(function (position) {
-	// var gps = (position.coords.latitude + "," + position.coords.longitude);
-	// window.gps = gps;
+	navigator.geolocation.getCurrentPosition(function (position) {
+		// var gps = (position.coords.latitude + "," + position.coords.longitude);
+		// window.gps = gps;
 
-	x = position.coords.latitude;
-	y = position.coords.longitude
+		x = position.coords.latitude;
+		y = position.coords.longitude
 
-	continueSomeProcess()
-});
-//creating callback function  so my geolocation loads before any of my javascipt runs.
-function continueSomeProcess() {
-
-	console.log(x)
-
-
-
-
-
+		continueSomeProcess()
+	});
+	//creating callback function  so my geolocation loads before any of my javascipt runs.
+	function continueSomeProcess() {
+		console.log(x)
 		var gasStationURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + x + "," + y + "&radius=2000&types=convenience_store&limit=5&key=AIzaSyAiF9BD-SMgaRYtpi0vIEzyj_6vhO0t83o"
-
-
-
 
 		$.ajax({
 			url: gasStationURL,
@@ -50,23 +61,38 @@ function continueSomeProcess() {
 				for (var i = 0; i < 5; i++) {
 					var list = $("<li>");
 					list.addClass("list-group-item")
-					list.html(response.results[i].name + "<br>" + response.results[i].vicinity + ", Utah <br>")
+					list.html(response.results[i].name + "<br>" + response.results[i].vicinity + ", Utah <br>"+ "Cherry: " + cherry + "<br>" + "Vanilla: " + vanilla + "<br>" + "Coconut: " + coconut +" <br>" + "Peach: " + peach)
 
-					$(".gas-station-list").append(list);					
+					$(".gas-station-list").append(list);
 				}
 				initMap(x, y)
 			})
 
 
-	
+	}
+	//grabs data from firebase and re
+	function getSyrups() {
+		database.ref().on("child_added", function (childSnapshot) {
+			console.log(childSnapshot);
+			cherry = childSnapshot.val().cherry;
+			vanilla = childSnapshot.val().vanilla;
+			coconut = childSnapshot.val().coconut;
+			peach = childSnapshot.val().peach;
 
 
+			//  syrupDiv = $("<div>").addClass("syrup-data").append(
+			// 	$("<div>").addClass("table-data col-lg-2").text(cherry),
+			// 	$("<div>").addClass("table-data col-lg-2").text(vanilla),
+			// 	$("<div>").addClass("table-data col-lg-2").text(coconut),
+			// 	$("<div>").addClass("table-data col-lg-2").text(peach),
+			// )
 
-
-
-
-}
+				// $(".list-group-item").append(syrupDiv);
+		})
+	}
+	getSyrups()
 });
+
 
 // $(".carousel").carousel({
 // 	interval: 2000
--- assets/javascript/sodaForm.js
+++ assets/javascript/sodaForm.js
@@ -1,44 +1,102 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAu7V-H1lwTNT7OLJ3-5gmQhqDulenZZXo",
-    authDomain: "teamprojectrrc.firebaseapp.com",
-    databaseURL: "https://teamprojectrrc.firebaseio.com",
-    projectId: "teamprojectrrc",
-    storageBucket: "teamprojectrrc.appspot.com",
-    messagingSenderId: "850694117642"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCFaXvnaHL3JsKa8rsUtkAxFiUJBsPm9bQ",
+    authDomain: "stfirebase-66923.firebaseapp.com",
+    databaseURL: "https://stfirebase-66923.firebaseio.com",
+    projectId: "stfirebase-66923",
+    storageBucket: "stfirebase-66923.appspot.com",
+    messagingSenderId: "367723280740"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var cherry = "";
-  var vanilla = ""; 
-  var coconut = "";
-  var peach = "";
-  
-  $(".submit").on("click", function (event) {
-      event.preventDefault();
-      console.log("I'm working");
+var location = "c";
+var cherry = "";
+var vanilla = "";
+var coconut = "";
+var peach = "";
 
-      
-      //adding variables
-      cherry = $("#cherry-input").val();
-      vanilla = $("#vanilla-input").val();
-      coconut = $("#coconut-input").val();
-      peach = $("#peach-input").val();
+//calling geo locator like gasStationList.js but using to populate select element class gas-station-input with gas station locations in option elements
+// let x
+// let y
+// function initMap(latX, lonY) {
+//     // The location of Uluru
+//     var uluru = { lat: latX, lng: lonY };
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), { zoom: 4, center: uluru });
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({ position: uluru, map: map });
+// }
 
-      console.log(cherry);
-      console.log(vanilla);
-      console.log(coconut);
-      console.log(peach);
+// $(document).ready(function() {
 
-      database.ref().set({
-          cherry: cherry,
-          vanilla: vanilla,
-          coconut: coconut,
-          peach: peach,
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
-      
-  });
+
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//         // var gps = (position.coords.latitude + "," + position.coords.longitude);
+//         // window.gps = gps;
+
+//         latX = position.coords.latitude;
+//         lonY = position.coords.longitude
+
+//         continueSomeProcess()
+//     });
+// });
+
+//     //creating callback function  so my geolocation loads before any of my javascipt runs.
+//     function continueSomeProcess() {
+//         console.log(latX)
+//         var gasStationURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latX + "," + lonY + "&radius=2000&types=convenience_store&limit=5&key=AIzaSyAiF9BD-SMgaRYtpi0vIEzyj_6vhO0t83o"
+
+//         $.ajax({
+//             url: gasStationURL,
+//             method: "GET"
+//         })
+//             .then(function (response) {
+//                 $('.loading-gif-wrapper').hide()
+//                 console.log(response);
+//                 for (var i = 0; i < 5; i++) {
+//                     var list = $("<li>");
+//                     list.addClass("list-group-item")
+//                     list.html(response.results[i].name + "<br>" + response.results[i].vicinity + ", Utah <br>" + "Cherry: " + cherry + "<br>" + "Vanilla: " + vanilla + "<br>" + "Coconut: " + coconut + " <br>" + "Peach: " + peach)
+
+//                     $(".gas-station-list").append(list);
+//                 }
+//                 initMap(latX, lonY)
+//             })
+//     }
+// var gasStationOption= $("<option>").addClass("gas-station-option").text("location");
+// $("#gas-station-input").append(gasStationOption);
+
+
+
+    $(".submit").on("click", function (event) {
+        event.preventDefault();
+        console.log("I'm working");
+
+
+        //adding variables
+        // location = $(".gas-station-option").val();
+        cherry = $("#cherry-input").val();
+        vanilla = $("#vanilla-input").val();
+        coconut = $("#coconut-input").val();
+        peach = $("#peach-input").val();
+        
+        // console.log(location);
+        console.log(cherry);
+        console.log(vanilla);
+        console.log(coconut);
+        console.log(peach);
+
+        database.ref().push({
+            // location: location,
+            cherry: cherry,
+            vanilla: vanilla,
+            coconut: coconut,
+            peach: peach,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+    });
 
